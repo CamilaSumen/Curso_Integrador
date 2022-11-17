3e88ceeca63d7816ceb0576f0948a542e5f6a612
camilasumen <camilatasu@icloud.com>
2022-11-17 13:57:00 -0500

    mostrar detalle producto

--- src/main/java/com/Project/Project/Controladores/HomeController.java
+++ src/main/java/com/Project/Project/Controladores/HomeController.java
@@ -1,15 +1,21 @@
 package com.Project.Project.Controladores;
 
 import com.Project.Project.Dao.Service.ProductoService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.slf4j.Logger;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductoService productoService;
@@ -44,17 +50,25 @@ public class HomeController {
         return "usuario/register";
     }
 
-
     @GetMapping("/reservar")
     public String reservar(){
         return "usuario/reserva";
     }
 
+    @GetMapping("/contactar")
+    public String contacto(){
+        return "usuario/escribenos";
+    }
 
     @GetMapping("/carrito")
     public String vercarta(Model model){
         model.addAttribute("productos", productoService.findAll());
         return "usuario/carrito";
+    }
+    @GetMapping("/verproducto/{id}")
+    public String verproducto(@PathVariable Integer id){
+        log.info("Id producto enviado como parámetro {}", id);
+        return "usuario/verproducto";
     }
 
 }
