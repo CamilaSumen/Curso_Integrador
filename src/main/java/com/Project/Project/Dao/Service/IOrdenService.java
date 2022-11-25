7664e0afd8ee3ae169a889c87d2114b467896054
camilasumen <camilatasu@icloud.com>
2022-11-25 13:22:31 -0500

    método para generar el número de la orden

--- src/main/java/com/Project/Project/Dao/Service/IOrdenService.java
+++ src/main/java/com/Project/Project/Dao/Service/IOrdenService.java
@@ -2,6 +2,10 @@ package com.Project.Project.Dao.Service;
 
 import com.Project.Project.Modelo.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
+
+    List<Orden> findAll();
     Orden save (Orden orden);
 }
--- src/main/java/com/Project/Project/Dao/Service/OrdenServiceImpl.java
+++ src/main/java/com/Project/Project/Dao/Service/OrdenServiceImpl.java
@@ -5,6 +5,9 @@ import com.Project.Project.Modelo.Orden;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrdenServiceImpl implements IOrdenService {
 
@@ -12,7 +15,36 @@ public class OrdenServiceImpl implements IOrdenService {
     private IOrdenRepository ordenRepository;
 
     @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
+
+    @Override
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
+    }
+
+    //000010
+    public String generarNumeroOrden(){
+        int numero = 0;
+        String numeroConcatenado = "";
+
+        List<Orden> ordenes = findAll();
+        List<Integer> numeros = new ArrayList<Integer>();
+
+        ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+
+        if(ordenes.isEmpty()){
+            numero = 1;
+        } else{
+            numero = numeros.stream().max(Integer::compare).get();
+            numero++;
+        }
+        if (numero<10){ //0000001
+            numeroConcatenado = "000000"+String.valueOf(numero);
+        } else if(numero<100){
+            numeroConcatenado = "00000"+String.valueOf(numero);
+        }
+        return numeroConcatenado;
     }
 }
